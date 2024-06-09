dd784a1f9fe87ce5c2244ba50cc834785100c162
Daniel McCord <git@danielmccord.dev>
2024-06-09 11:21:56 -0700

    Get just the descriptions

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,6 +1,8 @@
 import { } from "@citation-js/plugin-wikidata";
 import { QueryEngine } from "@comunica/query-sparql";
 
+let authorDescriptions = []
+
 // https://stackoverflow.com/questions/29601839/standard-regex-to-prevent-sparql-injection/55726984#55726984
 function escapeSPARQL(str: string) {
   str.replace(/(["'\\])/g, "\\$1");
@@ -47,19 +49,10 @@ ORDER BY (UCASE(?relatedLabel))`;
   const result = (
     await queryEngine.queryBindings(query, { sources: ["https://query.wikidata.org/sparql"] })
   ).on("data", (binding) => {
-    console.log(binding.toString()); // Quick way to print bindings for testing
+    // console.log(binding.toString()); // Quick way to print bindings for testing
+    authorDescriptions.push(binding.get("description").value);
+  }).on("end", () => console.log(authorDescriptions));
 
-    // // Obtaining values
-
-    // console.log(binding.get('item').value);
-    // console.log(binding.get('itemLabel').termType);
-    // console.log(binding.get('p').value);
-    // console.log(binding.get('statement0').value);
-  });
-
-  // return result;
 }
 
-// const foo = await getAuthors("Douglas Adams");
-// getAuthors("Q19959618");
 getAuthors("Douglas Adams");
