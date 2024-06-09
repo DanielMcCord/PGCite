8207422bce615a31ae3000ce346d415ddca2dba7
connorcurn <172141432+connorcurn@users.noreply.github.com>
2024-06-09 00:53:38 -0700

    formatted sandbox.ts with biome

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,13 +1,13 @@
-import { } from "@citation-js/plugin-wikidata";
+import {} from "@citation-js/plugin-wikidata";
 import { QueryEngine } from "@comunica/query-sparql";
 // const SparqlParser = require('sparqljs').Parser;
 
 // https://stackoverflow.com/questions/29601839/standard-regex-to-prevent-sparql-injection/55726984#55726984
-const escapeForTurtle = (s: string) => s.replace(/(["'\\])/g, '\\$1')
+const escapeForTurtle = (s: string) => s.replace(/(["'\\])/g, "\\$1");
 
 async function getAuthors(name: string) {
-    const apiRootUrl = "";
-    const query = `
+  const apiRootUrl = "";
+  const query = `
         PREFIX wikibase: <http://wikiba.se/ontology#>
         PREFIX wd: <http://www.wikidata.org/entity/>
         PREFIX bd: <http://www.bigdata.com/rdf#>
@@ -25,24 +25,24 @@ SELECT DISTINCT ?related ?relatedLabel WHERE {
 }
 ORDER BY (UCASE(?relatedLabel))`;
 
-    const queryEngine = new QueryEngine();
-    // const result = await queryEngine.query(query);
+  const queryEngine = new QueryEngine();
+  // const result = await queryEngine.query(query);
 
-    let result = (await queryEngine.queryBindings(query, { sources: ["https://query.wikidata.org/sparql"] }))
-        .on('data', (binding) => {
-            console.log(binding.toString()); // Quick way to print bindings for testing
+  let result = (
+    await queryEngine.queryBindings(query, { sources: ["https://query.wikidata.org/sparql"] })
+  ).on("data", (binding) => {
+    console.log(binding.toString()); // Quick way to print bindings for testing
 
-            // // Obtaining values
+    // // Obtaining values
 
-            // console.log(binding.get('item').value);
-            // console.log(binding.get('itemLabel').termType);
-            // console.log(binding.get('p').value);
-            // console.log(binding.get('statement0').value);
-        })
+    // console.log(binding.get('item').value);
+    // console.log(binding.get('itemLabel').termType);
+    // console.log(binding.get('p').value);
+    // console.log(binding.get('statement0').value);
+  });
 
-    // return result;
+  // return result;
 }
 
-let foo = await getAuthors("Douglas Adams")
+let foo = await getAuthors("Douglas Adams");
 // console.log(foo)
-
