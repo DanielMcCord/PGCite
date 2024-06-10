bd0e2ef5c11e6f86093d722b4bc8456c8da7a9bb
connorcurn <172141432+connorcurn@users.noreply.github.com>
2024-06-10 15:28:12 -0700

    Major changes to functionality and minor changes to formatting

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,99 +1,117 @@
+import { } from "node:readline";
 import { } from "@citation-js/plugin-wikidata";
 import { QueryEngine } from "@comunica/query-sparql";
-import { } from "readline"
-
-let authorDescriptions = []
 
 // https://stackoverflow.com/questions/29601839/standard-regex-to-prevent-sparql-injection/55726984#55726984
 function escapeSPARQL(str: string) {
-  str.replace(/(["'\\])/g, "\\$1");
+  return str.replace(/(["'\\])/g, "\\$1");
 }
 
-async function getAuthors(id: string) {
-  const queryForAuthors = `
-PREFIX wikibase: <http://wikiba.se/ontology#>
-PREFIX bd: <http://www.bigdata.com/rdf#>
-SELECT ?id ?name ?description WHERE {
-SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-  VALUES ?name {
-    "${id}"@en
-  }
-  ?id rdfs:label ?name;
-    schema:description ?description.
-  FILTER((LANG(?name)) = "en")
-  FILTER((LANG(?description)) = "en")
-}
-`;
-
-  const queryForAuthorInfo = `
+// Make a request to the Wikidata SPAQL API, using a given SPARQL query (as it would be entered in https://query.wikidata.org/)
+// Returns an array of bindings (https://comunica.dev/docs/query/getting_started/query_app/#3-3-consuming-binding-results-as-an-array)
+async function makeRequest(query: string) {
+  const queryWithPrefixes = `
 PREFIX wikibase: <http://wikiba.se/ontology#>
 PREFIX wd: <http://www.wikidata.org/entity/>
 PREFIX bd: <http://www.bigdata.com/rdf#>
 PREFIX p: <http://www.wikidata.org/prop/>
 PREFIX ps: <http://www.wikidata.org/prop/statement/>
-SELECT DISTINCT ?related ?relatedLabel WHERE {
-  VALUES ?target {
-    wd:${id}
-  }
-  { ?target ?prop ?related. }
-  UNION
-  { ?related ?prop ?target. }
-  FILTER(CONTAINS(STR(?related), "/entity/Q"))
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-}
-ORDER BY (UCASE(?relatedLabel))`;
+${query}`;
 
-  const query = queryForAuthors;
-
-  const queryEngine = new QueryEngine();
-
-  const result = (
-    await queryEngine.queryBindings(query, { sources: ["https://query.wikidata.org/sparql"] })
-  ).on("data", (binding) => {
-    // console.log(binding.toString()); // Quick way to print bindings for testing
-    authorDescriptions.push({
-      "id": binding.get("id").value,
-      "description": binding.get("description").value
-    });
-  }).on("end", () => console.log(authorDescriptions));
-
-}
-
-
-async function getAuthorInfo(id: string) {
-
-const queryForAuthorInfo = `
-PREFIX wikibase: <http://wikiba.se/ontology#>
-PREFIX wd: <http://www.wikidata.org/entity/>
-PREFIX bd: <http://www.bigdata.com/rdf#>
-PREFIX p: <http://www.wikidata.org/prop/>
-PREFIX ps: <http://www.wikidata.org/prop/statement/>
-SELECT DISTINCT ?related ?relatedLabel WHERE {
-  VALUES ?target {
-    wd:${id}
-  }
-  { ?target ?prop ?related. }
-  UNION
-  { ?related ?prop ?target. }
-  FILTER(CONTAINS(STR(?related), "/entity/Q"))
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-}
-ORDER BY (UCASE(?relatedLabel))`;
-
-  const query = queryForAuthorInfo;
-
-  const queryEngine = new QueryEngine();
-
-  const result = (
-    await queryEngine.queryBindings(query, { sources: ["https://query.wikidata.org/sparql"] })
-  ).on("data", (binding) => {
-    console.log(binding.toString()); // Quick way to print bindings for testing
+  const bindingsStream = await new QueryEngine().queryBindings(queryWithPrefixes, {
+    sources: ["https://query.wikidata.org/sparql"],
   });
 
+  return await bindingsStream
+    .on("error", (error) => {
+      console.log(error);
+    })
+    .toArray();
 }
 
+class Person {
+  name: string;
+  description: string;
+  id: string;
 
-await getAuthors("William Carpenter");
-// await getAuthorInfo(authorDescriptions.filter(a => (a.description === "English author and humourist (1952–2001)"))[0])
+  constructor(name: string, description: string, id: string) {
+    this.name = name;
+    this.description = description;
+    this.id = id;
+  }
 
-getAuthorInfo("Q8006577");
+  toString() {
+    return `${this.id}: ${this.name} (${this.description})`;
+  }
+}
+
+// Get a list of authors with an exact name (e.g. "Douglas Adams")
+async function getAuthors(name: string) {
+  const queryForAuthors = `
+  SELECT ?id ?name ?description WHERE {
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+
+  VALUES ?name {
+    "${escapeSPARQL(name)}"@en
+  }
+
+  ?id rdfs:label ?name;
+    schema:description ?description.
+
+  FILTER((LANG(?name)) = "en")
+  FILTER((LANG(?description)) = "en")
+}`;
+
+  const result: Person[] = (await makeRequest(queryForAuthors)).map((binding) => {
+    const person = new Person(
+      binding.get("name").value,
+      binding.get("description").value,
+      binding.get("id").value,
+    );
+
+    return person;
+  });
+
+  return result;
+}
+
+class Field {
+  label: string;
+  value: string;
+
+  constructor(label: string, value: string) {
+    this.label = label;
+    this.value = value;
+  }
+
+  toString() {
+    return `${this.label}: ${this.value}`;
+  }
+}
+
+// Get information about a given author, using an exact ID (ex. Q42)
+async function getAuthorInfo(id: string) {
+  const query = `
+SELECT DISTINCT ?related ?relatedLabel WHERE {
+  VALUES ?target {
+    wd:${escapeSPARQL(id)}
+  }
+  { ?target ?prop ?related. }
+  UNION
+  { ?related ?prop ?target. }
+  FILTER(CONTAINS(STR(?related), "/entity/Q"))
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+}
+ORDER BY (UCASE(?relatedLabel))
+`;
+
+  const result: Field[] = (await makeRequest(query)).map((binding) => {
+    const field = new Field(binding.get("relatedLabel").value, binding.get("related").value);
+
+    return field;
+  });
+
+  return result;
+}
+
+console.log(await getAuthors("William Carpenter"), await getAuthorInfo("Q8006577"));
